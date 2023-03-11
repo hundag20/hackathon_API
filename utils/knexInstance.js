b7ea133880c9ab086751e6b3413e1fb431037d2f
hundag20 <hundaguluma@gmail.com>
2023-03-11 20:07:36 +0300

    set env path to .env

--- utils/knexInstance.js
+++ utils/knexInstance.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv");
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+dotenv.config({ path: `.env` });
 
 const knex = require("knex");
 exports.knexInstance = knex({
