5816d98da125d7de8e38d2964058e13c5c81b8b4
hundag20 <hundaguluma@gmail.com>
2023-03-11 20:30:30 +0300

    try logging

--- utils/knexInstance.js
+++ utils/knexInstance.js
@@ -1,13 +1,15 @@
 const dotenv = require("dotenv");
-// dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+const connString = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+logger("info", JSON.stringify(connString));
 const knex = require("knex");
+const logger = require("./logger");
 exports.knexInstance = knex({
   client: "mysql2",
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
+  connection: connString,
 });
